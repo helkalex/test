a33d9296f97231fb6ff5e096ea26a8f13e2857cb
H K Alexander <hkalexanderwebdeveloper@gmail.com>
2023-05-16 13:59:52 +0100

    Create project folder structure and add Transport page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,1 @@
 
-import './App.css';
-
-
-function App() {
-  return (
-      <>
-      <header className="App-header">Hello Me</header>
-   
-      </>
-  );
-}
-
-export default App;
-
-
-// import mapp from './mapp.png';
-//  <img src={mapp} alt="logo" />
--- src/Transport.js
+++ src/Transport.js
@@ -17,16 +17,11 @@ function Transport() {
 
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d272138.1865988398!2d-5.596256564311861!3d57.799999082559985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x488e442f5d900f99%3A0x90f0327752c6117a!2sAn%20Teallach!5e0!3m2!1sen!2suk!4v1684226624944!5m2!1sen!2suk" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     <p></p>
-{/* Need to add Google maps in here and adjust css styling */}
-
-
-
-
 
 <img src={mapp} alt="logo"></img>
 
 
-{/* Will need to add navigation bar here */}
+{/* Need to add navigation bar here */}
     </div>
   );
 }
