87ec9d35b67c104f42ae1e715ddae6ba0f0c83ca
helkalex <112626351+helkalex@users.noreply.github.com>
2023-05-16 12:41:09 +0100

    Add files via upload

---
+++ project/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+      <>
+      <header className="App-header">Hello Me</header>
+   
+      </>
+  );
+}
+
+export default App;
+
+
+// import mapp from './mapp.png';
+//  <img src={mapp} alt="logo" />
